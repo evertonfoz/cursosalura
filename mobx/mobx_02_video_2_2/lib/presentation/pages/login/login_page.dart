f768a36393c3016f4fef49102c09dfc6394f96a7
evertonfoz <evertoncoimbra@gmail.com>
2020-05-22 21:05:03 -0300

    Ajustes para plano de aula - Etapa 1 para vídeo 2.2

--- mobx/mobx_02_video_2_2/lib/presentation/pages/login/login_page.dart
+++ mobx/mobx_02_video_2_2/lib/presentation/pages/login/login_page.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:mobx02video21/presentation/pages/home/home_page.dart';
 
-import 'mobx/login_page_mobx.dart';
+import 'stores/login_page_store.dart';
 
 class LoginPage extends StatelessWidget {
-  // Remover este código da classe
-  final _loginPageMobx = LoginPageMobx();
-
-  // Código a ser implementado logo no início da classe
   final FocusNode _emailNode = FocusNode();
   final FocusNode _senhaNode = FocusNode();
+
+  // Implementar este código após a definição dos FocusNodes
+  final _loginPageStore = LoginPageStore();
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +23,6 @@ class LoginPage extends StatelessWidget {
           children: [
             TextField(
               keyboardType: TextInputType.emailAddress,
-              // Código a ser inserido no TextField de email
               textInputAction: TextInputAction.next,
               onSubmitted: (_) => FocusScope.of(context).nextFocus(),
               focusNode: _emailNode,
@@ -35,20 +32,20 @@ class LoginPage extends StatelessWidget {
                 ),
                 hintText: 'Informe o email',
               ),
-              // Código a ser inserido no widget TextField de email
-              onChanged: (value) => _loginPageMobx.atualizarEmail([value]),
+              // Atualizar onChanged do TextField de email
+              onChanged: (value) =>
+                  _loginPageStore.atualizarEmail(newValue: value),
             ),
-            // Encapsulamos o Visibility para email em um Observer, widget do flutter_mobx
             Observer(builder: (_) {
               return Visibility(
-                visible: !_oEmailEhValido(),
+                // Atualizar o visible do Visibility para mensagem de erro do email
+                visible: !_loginPageStore.oEmailEhValido,
                 child:
                     _mensagemDeErro(mensagem: 'Um email correto é obrigatório'),
               );
             }),
             TextField(
               keyboardType: TextInputType.text,
-              // Código a ser inserido no TextField de senha
               focusNode: _senhaNode,
               decoration: InputDecoration(
                 prefixIcon: Icon(
@@ -56,24 +53,23 @@ class LoginPage extends StatelessWidget {
                 ),
                 hintText: 'Informe a senha',
               ),
-              // Código a ser inserido no widget TextField de senha
-              onChanged: (value) => _loginPageMobx.atualizarSenha([value]),
+              // Atualizar onChanged do TextField de senha
+              onChanged: (value) =>
+                  _loginPageStore.atualizarSenha(newValue: value),
             ),
-            // Encapsulamos o Visibility para senha em um Observer, widget do flutter_mobx
             Observer(builder: (_) {
               return Visibility(
-                visible: !_aSenhaEhValida(),
+                // Atualizar o visible do Visibility para mensagem de erro da senha
+                visible: !_loginPageStore.aSenhaEhValida,
                 child: _mensagemDeErro(mensagem: 'A senha é obritatória'),
               );
             }),
             Padding(
               padding: const EdgeInsets.only(top: 15),
-              // Encapsulamos o RaisedButton pem um Observer, widget do flutter_mobx
               child: Observer(builder: (_) {
                 return RaisedButton(
                   child: Text('Acessar'),
-                  // Adaptação do onPressed de RaisedButton para enviar o contexto ao método _oFormularioEhValido
-                  onPressed: _oFormularioEhValido(context: context),
+                  onPressed: _loginPageStore.oFormularioEhValido,
                 );
               }),
             ),
@@ -83,7 +79,6 @@ class LoginPage extends StatelessWidget {
     );
   }
 
-  // Método a ser implementado após o build()
   _mensagemDeErro({String mensagem}) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.end,
@@ -100,34 +95,15 @@ class LoginPage extends StatelessWidget {
     );
   }
 
-  // Métodos a serem implementados após o _mensagemDeErro
-  _oEmailEhValido() {
-    // Adaptar referência ao valor informado para o email no método _oEmailEhValido
-    if (_loginPageMobx.email.value.trim().length == 0) return false;
-
-    Pattern pattern =
-        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
-    RegExp regex = new RegExp(pattern);
-    // Adaptar referência ao valor informado para o email no método _oEmailEhValido
-    return (regex.hasMatch(_loginPageMobx.email.value));
-  }
-
-  _aSenhaEhValida() {
-    // Adaptar referência ao valor informado para a senha no método _aSenhaEhValida
-    return _loginPageMobx.senha.value.trim().length > 0;
-  }
-
-  // Adaptação da assinatura do método _oFormularioEhValido
-  _oFormularioEhValido({BuildContext context}) {
-    if (_oEmailEhValido() && _aSenhaEhValida()) {
+  _onPressedParaBotaoAcessar({BuildContext context}) {
+    if (_loginPageStore.oFormularioEhValido)
       return () => Navigator.push(
             context,
             MaterialPageRoute(
               builder: (context) => HomePage(),
             ),
           );
-    } else {
+    else
       return null;
-    }
   }
 }
